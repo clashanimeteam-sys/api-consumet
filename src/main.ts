cb09a43fe0b416ba7384c7e482a9f13752c0d273
clashanimeteam-sys <clashanime.team@gmail.com>
2026-07-11 12:59:58 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,8 @@ import Fastify from 'fastify';
 import FastifyCors from '@fastify/cors';
 import fs from 'fs';
 
-// import books from './routes/books';
 import anime from './routes/anime';
-// import manga from './routes/manga';
-// import comics from './routes/comics';
-// import lightnovels from './routes/light-novels';
-// import movies from './routes/movies';
-import meta from './routes/meta';
-// import news from './routes/news';
 import chalk from 'chalk';
-import Utils from './utils';
 
 export const redis =
   process.env.REDIS_HOST &&
@@ -27,7 +19,7 @@ const fastify = Fastify({
   maxParamLength: 1000,
   logger: true,
 });
-export const tmdbApi = process.env.TMDB_KEY && process.env.TMDB_KEY;
+
 (async () => {
   const PORT = Number(process.env.PORT) || 3000;
 
@@ -112,21 +104,8 @@ export const tmdbApi = process.env.TMDB_KEY && process.env.TMDB_KEY;
   console.log(chalk.green(`Starting server on port ${PORT}... 🚀`));
   if (!process.env.REDIS_HOST)
     console.warn(chalk.yellowBright('Redis not found. Cache disabled.'));
-  if (!process.env.TMDB_KEY)
-    console.warn(
-      chalk.yellowBright('TMDB api key not found. the TMDB meta route may not work.'),
-    );
 
-  // await fastify.register(books, { prefix: '/books' });
   await fastify.register(anime, { prefix: '/anime' });
-  // await fastify.register(manga, { prefix: '/manga' });
-  // await fastify.register(comics, { prefix: '/comics' });
-  // await fastify.register(lightnovels, { prefix: '/light-novels' });
-  // await fastify.register(movies, { prefix: '/movies' });
-  await fastify.register(meta, { prefix: '/meta' });
-  // await fastify.register(news, { prefix: '/news' });
-
-  await fastify.register(Utils, { prefix: '/utils' });
 
   try {
     fastify.get('/', (_, rp) => {
@@ -154,6 +133,7 @@ export const tmdbApi = process.env.TMDB_KEY && process.env.TMDB_KEY;
     process.exit(1);
   }
 })();
+
 export default async function handler(req: any, res: any) {
   await fastify.ready();
   fastify.server.emit('request', req, res);
